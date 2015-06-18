f7f898a1023e00c69ff06b9980e3535072dc09d1
Mehdi <mehdi@abtasty.com>
2015-06-18 17:20:58 +0200

    add dataProvider

--- src/controllers/headerController.js
+++ src/controllers/headerController.js
@@ -1,20 +1,5 @@
 'use strict';
 
-var data_languages = [
-	{ 'id': 1 , 'tag': 'fr' , 'country': 'France' , 'name': 'Français' , 'url': '/' , 'class': 'flag-fr' , 'active': false},
-	{ 'id': 2 , 'tag': 'en' , 'country': 'United States' , 'name': 'English (US)' , 'url': '/' , 'class': 'flag-us' , 'active': true},
-	{ 'id': 3 , 'tag': 'es' , 'country': 'España' , 'name': 'Español' , 'url': '/' , 'class': 'flag-es' , 'active': false},
-	{ 'id': 4 , 'tag': 'de' , 'country': 'German' , 'name': 'Deutsch' , 'url': '/' , 'class': 'flag-de' , 'active': false}
-];
-
-var data_news = [
-	{ 'title': 'Stat Wars, Episode VIII: Return of Bayes' },
-	{ 'title': 'Comment mesurer l’impact de sa stratégie de testing sur le long terme ?' },
-	{ 'title': 'L’AB Testing, nouveau relais de croissance des ecommerçants ?' },
-	{ 'title': '6 astuces pour optimiser son site en cette période de fête' }
-	
-];
-
 app
 	.controller('headerController' , function ($scope) {
 		$scope.languages = data_languages;
--- src/controllers/sidebarController.js
+++ src/controllers/sidebarController.js
@@ -1,25 +1,8 @@
 'use strict';
 
-var data_menu = [
-	{'id': 1, 'name': 'Dashboard'             , 'url': '/'                 , 'fa_icon': 'fa-home'        , 'active': true},
-	{'id': 2, 'name': 'Knowledge'             , 'url': '/knowledge'        , 'fa_icon': 'fa-cloud'       , 'active': false},
-	{'id': 3, 'name': 'Client stats'          , 'url': '/clientstats'      , 'fa_icon': 'fa-bar-chart'   , 'active': false},
-	{'id': 4, 'name': 'Pending tests'         , 'url': '/pendingtests'     , 'fa_icon': 'fa-play-circle' , 'active': false},
-	{'id': 4, 'name': 'Translations'          , 'url': '/translations'     , 'fa_icon': 'fa-comments-o'  , 'active': false},
-	{'id': 4, 'name': 'Validate plugins'      , 'url': '/validateplugins'  , 'fa_icon': 'fa-plug'        , 'active': false},
-	{'id': 4, 'name': 'Translate plugins'     , 'url': '/translateplugins' , 'fa_icon': 'fa-comment-o'   , 'active': false},
-	{'id': 4, 'name': 'Last 24h tests ranking', 'url': '/lastTestsRanking' , 'fa_icon': 'fa-signal'      , 'active': false},
-	{'id': 4, 'name': 'Webinar management'    , 'url': '/webinar'          , 'fa_icon': 'fa-calendar'    , 'active': false}
-];
-var data_user = {
-		'firstname': 'mehdi',
-		'lastname' : 'betari',
-		'imgUri'   : 'ressources/img/avatars/mehdi.jpg',
-	};
-
 app
-	.controller('sidebarController' , function ($scope) {
-		$scope.menu = data_menu;
-		$scope.user = data_user;
+	.controller('sidebarController' , function ($scope , dataProvider) {
+		$scope.menu = dataProvider.getMenu();
+		$scope.user = dataProvider.getUser();
 	})
 ;
